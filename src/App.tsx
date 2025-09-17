1716491372506c0262c371b2a67f2619e2b05055
Batyr <awbatyr@gmail.com>
2025-09-17 16:00:17 +0500

    Add Toaster component for improved notification handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Footer, Header } from "./components/layout";
 import { About, ContactForm, Hero, Services } from "./components/sections";
 import { LanguageSync } from "./lang-sync";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <About />
       <Services />
       <ContactForm />
+      <Toaster containerClassName="font-sans" />
       <Footer />
     </main>
   );
